b61d2e551fa49f9c92c09f598bb04c48a3a81364
yoo <s_plus7@naver.com>
2022-04-29 09:14:59 +0900

    first commit

---
+++ mavenWebDemo/src/test/java/com/ysy/ysyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ysy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ysyApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
